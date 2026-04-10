b58ed6bc9f66e1d5fd83cc005963d5ca34ba63ae
Joshua Schmidt <joshua@ka1schmidt.de>
2026-04-10 22:27:14 +0200

    regreeter module

---
+++ modules/greeter.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+
+{
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = ''
+          ${pkgs.cage}/bin/cage -s -- ${pkgs.regreet}/bin/regreet
+        '';
+        user = "greeter";
+      };
+    };
+  };
+  programs.regreet = {
+    enable = true;
+    theme = {
+      package = pkgs.adw-gtk3;
+      name = "adw-gtk3-dark";
+    };
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus-Dark";
+    };
+    cursorTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+    font = {
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "JetBrainsMono Nerd Font";
+      size = 11;
+    };
+  };
+  environment.systemPackages = with pkgs; [
+    cage
+  ];
+}
