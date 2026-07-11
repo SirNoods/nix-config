8d1bbb1ed273c2dd1db818943cda09ce6f23236e
goshva <goshva@goshva.cool>
2026-07-12 01:47:14 +0200

    finally, a greeter theme

--- modules/greeter/sddm.nix
+++ modules/greeter/sddm.nix
@@ -1,3 +1,27 @@
+{ pkgs, ... }:
+
+let
+  whereIsMySddmClassic =
+    pkgs.where-is-my-sddm-theme.overrideAttrs
+      (old: {
+        postInstall = (old.postInstall or "") + ''
+          cp \
+            "$src/where_is_my_sddm_theme/example_configurations/classic_nocursor.conf" \
+            "$out/share/sddm/themes/where_is_my_sddm_theme/theme.conf"
+        '';
+      });
+in
 {
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    theme = "where_is_my_sddm_theme";
+
+    extraPackages = with pkgs; [
+      qt6Packages.qt5compat
+    ];
+  };
+
+  environment.systemPackages = [
+    whereIsMySddmClassic
+  ];
 }
